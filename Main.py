ba7d4e8e104b2e91d8f7638707a89b124040ceec
Ando Pepe <coolcatando@gmail.com>
2024-09-17 16:39:18 -0500

    removed rooms and made everything class based for the space

--- Main.py
+++ Main.py
@@ -1,55 +1,90 @@
-import numpy as np
-# Class to represent a room in the space
-class Room:
+from enum import Enum
+# class syntax
+class Actions(Enum):
+    LEFT = 1
+    RIGHT = .9
+    UP = .8
+    DOWN = .7
+    SUCK = .6
 
-    def __init__(self, isDirty, hasVacuum):
-        self.isDirty = isDirty
-        self.hasVacuum = hasVacuum
+class Space:
+    def __init__(self, rows, columns, vacuumStartLoc, dirty_squares):
+        self.rows = rows
+        self.columns = columns
+        self.goalCost = 0
+        self.vacLoc = vacuumStartLoc
+        self.dirtLocs = dirty_squares
 
-def printFloorLayout(array):
-    """
-    Displays the layout of the space
-    each sell has format (row, colum)
-    * If the space contains the vacuum it will additionally display 'Vac'
-    * If the space is dirty it will additionally display 'Dirty'
-    """
-    matrix = np.array(array)
-    
-    dimensions = matrix.shape
-    rows, columns = dimensions
+    def printFloorLayout(self):
+        """
+        Displays the layout of the space
+        each sell has format (row, colum)
+        * If the space contains the vacuum it will additionally display 'Vac'
+        * If the space is dirty it will additionally display 'Dirty'
+        """
 
-    for i in range(rows):
-        for j in range(columns):
-            text = "[({0}, {1}) {2:3} {3:5}]"
-            print(text.format(i+1, j+1, ("", "Vac")[array[i][j].hasVacuum], ("", "Dirty")[array[i][j].isDirty]), end="")
-            if j != columns - 1:
-                print(", ", end="") 
-        print("")
+        for i in range(self.rows):
+            for j in range(self.columns):
+                text = "[({0}, {1}) {2:3} {3:5}]"
+                print(text.format(i+1, j+1, ("", "Vac")[(i+1,j+1)==self.vacLoc], ("", "Dirty")[(i+1, j+1) in self.dirtLocs]), end="")
+                if j != self.columns - 1:
+                    print(", ", end="") 
+            print("")
 
-def printFloorState(array):
-    """
-    Displays which room has the vacuum and which rooms are dirty
-    """
-    matrix = np.array(array)
-    
-    dimensions = matrix.shape
-    rows, columns = dimensions
-    dirtyRooms = []
-    for i in range(rows):
-        for j in range(columns):
-            if (array[i][j].hasVacuum):
-                vacCords = (i+1,j+1)
-            if (array[i][j].isDirty):
-                dirtyRooms.append((i+1,j+1))
+    def printFloorState(self):
+        """
+        Displays which room has the vacuum and which rooms are dirty
+        """
+
+        print("[V", self.vacLoc, end="")
+        
+        if(self.dirtLocs):
+            for pair in self.dirtLocs:
+                print(", d"+str(pair), end="")
+        print("] with g = " + str(self.goalCost))
+
+    def performAction(self, action: Actions):
+        # print(action.name, action.value)
+        self.goalCost -= action.value
+
+        match action.name:
+            case 'LEFT':
+                if(self.vacLoc[1] != 1):
+                    self.vacLoc = (self.vacLoc[0], self.vacLoc[1] - 1)
+                    print("Moved "+action.name+" to " + str(self.vacLoc))
+                else:
+                    print("Didn't perform action " + action.name)
+
+            case 'RIGHT':
+                if(self.vacLoc[1] != self.columns):
+                    self.vacLoc = (self.vacLoc[0], self.vacLoc[1] + 1)
+                    print("Moved "+action.name+" to " + str(self.vacLoc))
+                else:
+                    print("Didn't perform action " + action.name)
+
+            case 'UP':
+                if(self.vacLoc[0] != 1 ):
+                    self.vacLoc = (self.vacLoc[0] - 1, self.vacLoc[1])
+                    print("Moved "+action.name+" to " + str(self.vacLoc))
+                else:
+                    print("Didn't perform action " + action.name)
 
 
-    print("[V", vacCords, end="")
-    
-    if(dirtyRooms):
-        for pair in dirtyRooms:
-            print(", d"+str(pair), end="")
-    print("]")
-            
+            case 'DOWN':
+                if(self.vacLoc[0] != 1 != self.rows):
+                    self.vacLoc = (self.vacLoc[0]+1, self.vacLoc[1]) 
+                    print("Moved "+action.name+" to " + str(self.vacLoc))
+                else:
+                    print("Didn't perform action " + action.name)
+
+
+            case 'SUCK':
+                if(self.vacLoc in self.dirtLocs):
+                    self.dirtLocs.remove(self.vacLoc)
+                    print("Cleaned " + str(self.vacLoc))
+                else:
+                    print("Didn't perform action " + action.name)
+        
 
 def setSpace(rows: int, columns: int, vacuumStartLoc, dirty_squares):
     """
@@ -79,15 +114,16 @@ def setSpace(rows: int, columns: int, vacuumStartLoc, dirty_squares):
     return array
 
 def main():
-    instance1 = setSpace(4, 5, (2,2), [(1,2),(2,4),(3,5)])
-    printFloorLayout(instance1)
-    printFloorState(instance1)
-     
+    instance1 = Space(4, 5, (2,2), [(1,2),(2,4),(3,5)])
+    instance1.printFloorLayout()
+    instance1.printFloorState()
+    instance1.performAction(Actions.UP)
+
     print("\n\n")
 
-    instance2 = setSpace(4, 5, (3,2), [(1,2),(2,1),(2,4),(3,3)])
-    printFloorLayout(instance2)
-    printFloorState(instance2)
+    instance2 = Space(4, 5, (3,2), [(1,2),(2,1),(2,4),(3,3)])
+    instance2.printFloorLayout()
+    instance2.printFloorState()
 
 if(__name__ == "__main__"):
     main()
