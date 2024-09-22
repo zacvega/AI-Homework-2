e800b81acc364a28598bcab7ae3da9143fcaa94a
MakingStuffandThings <92829622+MakingStuffandThings@users.noreply.github.com>
2024-09-22 18:42:51 -0500

    uniform graph cost

--- Main.py
+++ Main.py
@@ -189,7 +189,7 @@ class Space:
                 if(self.vacLoc in self.dirtLocs):
                     return True
         return False
-
+    
 # function Depth-Limited-Search( problem,limit) returns soln/fail/cutoff
 # Recursive-DLS(Make-Node(Initial-Stat e [problem]),problem,limit)
 # function Recursive-DLS(node,problem,limit) returns soln/fail/cutoff
@@ -279,50 +279,122 @@ def uniform_cost_tree_search(problem):
         fringe = fringe + newNodes
         fringe.sort(reverse=True)
 
+def uniform_cost_graph_search(problem):
+    fringe = [Node(problem)]
+    closed = set()
+    first5nodes = list()
+    expandedCount = 0
+    while True:
+        if (len(fringe) == 0):
+            node.result = False
+            return False, expandedCount, len(fringe), first5nodes
+        node = fringe.pop()
+        if goal_test(node):
+            node.result = True
+            return node, expandedCount, len(fringe), first5nodes
+        
+
+
+
+        if node.state not in closed:
+            closed.add(node.state)
+            expandedCount +=1
+            newNodes = expand(node)
+            if (len(first5nodes) < 5):
+                for i in newNodes:
+                    if (len(first5nodes) >= 5):
+                        break
+                    else:    
+                        first5nodes.append(i)
+            fringe = fringe + newNodes
+            fringe.sort(reverse=True)
+
+        
+        
+        
+        
+        
+
+
+
+
+    return 
+
+
 def main():
     instance1 = Space((2,2), [(1,2),(2,4),(3,5)], 4, 5)
     instance2 = Space((3,2), [(1,2),(2,1),(2,4),(3,3)], 4, 5)
 
-    # the following 2 instances set up by limiting the space to only whats needed
-    # determined by the max location used between vacuum or dirty spots
-    # instance1 = Space((2,2), [(1,2),(2,4),(3,5)])
-    # instance2 = Space((3,2), [(1,2),(2,1),(2,4),(3,3)])
-##################################################################################################################################################
-    # print("***************uniform cost graph search**********************")
-    # print("instance 1: uniform cost tree search")
-    # start = time.time()
-    # successNode, expanded, generated, first5nodes = uniform_cost_tree_search(copy.deepcopy(instance1))
-    # end = time.time()
-    # print(successNode)
-    # print("\tGenerated node count:", generated)
-    # print("\tFirst 5 nodes generated")
-    # for i in first5nodes:
-    #     print(f"\t\tMovement: {i.actions}, State: ", end="")
-    #     i.state.printFloorState()
-    # print("\tExpanded node count:", expanded)
-    # print(f"\tTook {end-start:.2f} seconds")
+    #the following 2 instances set up by limiting the space to only whats needed
+    #determined by the max location used between vacuum or dirty spots
+    instance1 = Space((2,2), [(1,2),(2,4),(3,5)])
+    instance2 = Space((3,2), [(1,2),(2,1),(2,4),(3,3)])
+#################################################################################################################################################
+    print("***************uniform cost graph search**********************")
+    print("instance 1: uniform cost tree search")
+    start = time.time()
+    successNode, expanded, generated, first5nodes = uniform_cost_tree_search(copy.deepcopy(instance1))
+    end = time.time()
+    print(successNode)
+    print("\tGenerated node count:", generated)
+    print("\tFirst 5 nodes generated")
+    for i in first5nodes:
+        print(f"\t\tMovement: {i.actions}, State: ", end="")
+        i.state.printFloorState()
+    print("\tExpanded node count:", expanded)
+    print(f"\tTook {end-start:.2f} seconds")
 
-    # print("\ninstance2: uniform cost tree search")
-    # start2 = time.time()
-    # successNode2, expanded2, generated2, first5nodes2 = uniform_cost_tree_search(copy.deepcopy(instance2))
-    # end2 = time.time()
-    # print(successNode2)
-    # print("\tGenerated node count:", generated2)
-    # print("\tFirst 5 nodes generated")
-    # for i in first5nodes2:
-    #     print(f"\t\tMovement: {i.actions}, State: ", end="")
-    #     i.state.printFloorState()
-    # print("\tExpanded node count:", expanded2)
-    # print(f"\tTook {end2-start2:.2f} seconds")
+    print("\ninstance2: uniform cost tree search")
+    start2 = time.time()
+    successNode2, expanded2, generated2, first5nodes2 = uniform_cost_tree_search(copy.deepcopy(instance2))
+    end2 = time.time()
+    print(successNode2)
+    print("\tGenerated node count:", generated2)
+    print("\tFirst 5 nodes generated")
+    for i in first5nodes2:
+        print(f"\t\tMovement: {i.actions}, State: ", end="")
+        i.state.printFloorState()
+    print("\tExpanded node count:", expanded2)
+    print(f"\tTook {end2-start2:.2f} seconds")
 
-    # print(f"\nBoth instances in total took {end2-start:.2f} seconds")
+    print(f"\nBoth instances in total took {end2-start:.2f} seconds")
 
 
-    # tstInstance.printFloorLayout()
+    #tstInstance.printFloorLayout()
     print("***************************************************************")
 ##################################################################################################################################################
     print("***************uniform cost graph search**********************")
+    print("instance 1: uniform cost graph search")
+    start = time.time()
+    successNode, expanded, generated, first5nodes = uniform_cost_graph_search(copy.deepcopy(instance1))
+    end = time.time()
+    print(successNode)
+    print("\tGenerated node count:", generated)
+    print("\tFirst 5 nodes generated")
+    for i in first5nodes:
+         print(f"\t\tMovement: {i.actions}, State: ", end="")
+         i.state.printFloorState()
+    print("\tExpanded node count:", expanded)
+    print(f"\tTook {end-start:.2f} seconds")
 
+    print("\ninstance2: uniform cost graph search")
+    start2 = time.time()
+    successNode2, expanded2, generated2, first5nodes2 = uniform_cost_graph_search(copy.deepcopy(instance2))
+    end2 = time.time()
+    print(successNode2)
+    print("\tGenerated node count:", generated2)
+    print("\tFirst 5 nodes generated")
+    for i in first5nodes2:
+         print(f"\t\tMovement: {i.actions}, State: ", end="")
+         i.state.printFloorState()
+    print("\tExpanded node count:", expanded2)
+    print(f"\tTook {end2-start2:.2f} seconds")
+
+    print(f"\nBoth instances in total took {end2-start:.2f} seconds")
+
+
+    
+    
 
 
 
